f0c3663578b46ce99a12dfb41e6d0476269626b9
itayAGEERA <itay@ageera.org>
2023-10-31 08:00:44 +0200

    Periodic results for c=4, very good plots

--- spatial_ecoevo_v1.0.0/ecoevo_original/multi_run.bash
+++ spatial_ecoevo_v1.0.0/ecoevo_original/multi_run.bash
@@ -10,11 +10,11 @@ small_time=$((-1*(10**5)))
 large_time=$((-1*(10**8)))
 final_time=$((2*(10**6))) 
 
-for ((i=1; i<=8; i++)); do
+for ((i=1; i<=16; i++)); do
    seed_array[i]=$((3688*i+2**i))
 done
 
-for ((i=1; i<=1; i++)); do
+for ((i=9; i<=16; i++)); do
   for ((c=4; c<=4; c++)); do
   	./run.bash "${seed_array[$i]}#"$run_name"_cycles"$c"_seed"${seed_array[$i]}"_AbsSin_"$Cmax"_"$Cmin"#$vbar#$dbar#$c#FALSE#$Cmax#$Cmin#$small_time#$large_time#$final_time"
 	done
